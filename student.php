5020c69fb835b4267f307aa0338209050e06c3ef
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-03-28 23:01:23 +0300

    change the href style location

--- student.php
+++ student.php
@@ -17,7 +17,7 @@
 		<!-- my css style sheet -->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 		<link rel="stylesheet" href="assets/css/styles.css">
-        <link rel="stylesheet" href="student.css">
+        <link rel="stylesheet" href="style/student.css">
 
 		
 		
