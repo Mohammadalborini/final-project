0636854b9f8d93e174b22bc61aab59dbf34a94ab
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-04-02 21:47:23 +0300

    add code login new

--- login.php
+++ login.php
@@ -1,92 +1,50 @@
 <?php
 
-// Initialize the session
 session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: student.php");
-    exit;
-}
- 
-// Include config file
-require_once "coniction.php";
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = $login_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT Student_id, Password FROM student WHERE Student_id = ?";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                // NOT WORKING HERE
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["Student_id"] = $id;
-                                                  
-                            
-                            // Redirect user to welcome page
-                            header("location: student.php");
-                        } else{
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.";
-                        }
-                    }
-                } else{
-                    // Username doesn't exist, display a generic error message
-                    $login_err = "Invalid username or password.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
 
-            // Close statement
-            mysqli_stmt_close($stmt);
+include("coniction.php");
+
+
+if($_SERVER['REQUEST_METHOD']== "POST")
+{
+	//something was posted
+	$username= $_POST['username'];
+	$password= $_POST['password'];
+	
+
+	if(($username)&&!empty($password))
+{
+	//read from the database
+	$query="select * from student where Student_id = '$username' limit 1";
+
+	$result = mysqli_query($con,$query);
+
+    if($result && mysqli_num_rows($result) > 0)
+    {
+        $user_data =mysqli_fetch_assoc($result);
+        
+        if($user_data['Password']===$password)
+        {
+            $_SESSION['user_id'] = $user_data['user_id'];
+            header("Location: student.php");
+            die;
+        }else{
+            $error = "Wrong username or password";
+            header("Location: WISE.php");
+          
         }
+    }else
+    {
+        $error = "Wrong username or password";
     }
-    
-    // Close connection
-    mysqli_close($con);
+
 }
+else
+{
+	$error = "please enter valid data";
+}
+
+}
+
 
 ?>
