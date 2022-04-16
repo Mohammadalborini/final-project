993c58536b65c2500191c968508bffc54930606d
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-04-16 18:52:49 +0300

    edit the website

--- e-practical training/Clever Mind POB.php
+++ e-practical training/Clever Mind POB.php
@@ -10,7 +10,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>blueneek</title>
+      <title>Clever Mind POB</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
@@ -27,15 +27,16 @@
       <!-- Tweaks for older IEs-->
       <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <link rel="icon" href="images/icon1.png">
       <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
       <style>
          .footer{
-   background: #00aeef;
-   position:relative;
-   bottom:0;
-   width:100%;
+            background: #00aeef;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
 }
       </style>
    </head>
@@ -56,7 +57,7 @@
                      <div class="full">
                         <div class="center-desk">
                            <div class="logo">
-                              <a href="index.html"><img src="images/header icon.png" alt="#" /></a>
+                              <a href="index.html"><img src="images/icon1.png" alt="#" style="width:200px;"/></a>
                            </div>
                         </div>
                      </div>
@@ -103,27 +104,6 @@
                      <h2>Reqeste A call back</h2>
                   </div>
                </div>
-               <div class="col-md-6 offset-md-3">
-                  <form id="request" class="main_form">
-                     <div class="row">
-                        <div class="col-md-12 ">
-                           <input class="contactus" placeholder="Full Name" type="type" name="Full Name"> 
-                        </div>
-                        <div class="col-md-12">
-                           <input class="contactus" placeholder="Phone Number" type="type" name="Phone Number"> 
-                        </div>
-                        <div class="col-md-12">
-                           <input class="contactus" placeholder="Email" type="type" name="Email">                          
-                        </div>
-                        <div class="col-md-12">
-                           <textarea class="contactus" placeholder="Message" type="type" Message="Name">Message </textarea>
-                        </div>
-                        <div class="col-sm-12">
-                           <button class="send_btn">Send</button>
-                        </div>
-                     </div>
-                  </form>
-               </div>
             </div>
          </div>
       </div>
@@ -135,7 +115,7 @@
                <div class="container">
                   <div class="row">
                      <div class="col-md-12">
-                        <p>© 2019 All Rights Reserved. <a href="https://html.design/"> Free Html Templates</a></p>
+                     <p>© 2022 All Rights Reserved. <a href="https://www.wise.edu.jo/"> The World Islamic Sciences and Education University</a></p>
                      </div>
                   </div>
                </div>
