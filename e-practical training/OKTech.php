a0685302b6eff54c55080c52c19d055271d15700
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-08 02:44:23 +0300

    add files

---
+++ e-practical training/OKTech.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="OKTech/Images/Images/Website Logo.png" type="image/png">
+	<title>OKTech</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="OKTech/css/bootstrap.css">
+	<link rel="stylesheet" href="OKTech/vendors/linericon/style.css">
+	<link rel="stylesheet" href="OKTech/css/font-awesome.min.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="OKTech/css/style.css">
+</head>
+
+<body>
+
+	<!--================Header Menu Area =================-->
+	<header class="header_area">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand-logo_h" href="OKTech.php"><img src="OKTech/Images/Images/Website Logo.png" alt=""></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav justify-content-center">
+							<li class="nav-item active"><a class="nav-link" href="OKTech.php">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="#About-us">About Us</a></li>
+							<li class="nav-item"><a class="nav-link" href="#Our-Server">Our Serves</a></li>
+							<li class="nav-item submenu dropdown">
+								<a href="#Our-Works" class="nav-link dropdown-toggle">Our Works</a>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#Our-Team" class="nav-link dropdown-toggle">Our Team</a>
+							</li>
+							<li class="nav-item"><a class="nav-link" href="#Contact-US">Contact US</a></li>
+							
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--================Header Menu Area =================-->
+	
+	<!--================Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5">
+						<div class="banner_content">
+                            <p>OKTech started its journey in 2017 in Jordan by specialists and experts in information technology</p>
+							<h2>
+								OKTech
+							</h2>
+				
+							<div class="d-flex align-items-center">
+								<a class="primary_btn" href="#About-us"><span>More Information</span></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-7">
+						<div class="home_right_img">
+							<img class="img-fluid" src="OKTech/img/banner/home-right.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Home Banner Area =================-->
+
+	<!--================Start Features Area =================-->
+	<div id="About-us">
+	<section class="section_gap features_area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<p class="top_title">We Have Exclusive Stunning Features</p>
+						<h2>About Us</h2>
+                        <h1>Description</h1>
+						<p style="color: black;">Knowledge Oriented Technology and Electronic Solutions Company OKTech launched its journey in 2017 in Jordan by specialists and experts in information technology and programming who always put innovation in providing integrated solutions in the field of programming web systems in general and ERP systems in particular in order to provide business owners with assistance in business management .
+
+                            OKTech is distinguished by programming the systems required by customers, special programming from scratch without using any ready-made models or tools, which meets all customer requests without restrictions.
+							</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<div class="left_features">
+						<img class="img-fluid" src="OKTech/Images/Images/Website Logo.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-5 offset-lg-1">
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+
+							<h4>The Courses</h4>
+                            <p style="color: black;">
+                                NETWORK basics course<br/>
+                                CCNA Network Design and Management Course<br/>
+                                MCSA Server Systems and Management Course<br/>
+                                MICROSOFT EXCHANGING SERVER 2012 Course<br/>
+                                Computer maintenance, software + hardware course<br/>
+                                CCNA Network Design and Management Course<br/>
+                                CCNA+ SECURITY . Network Security and Protection Course<br/>
+                                KALI LINUX Penetration Testing Systems Course<br/>
+                                CEH Ethical Penetration Testing Course
+                            </p>
+						</div>
+
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+							<h4>Certificates</h4>
+                            <p style="color: black;">
+                                Issuing accredited certificates from Al-Balqa Applied University and the Ministry of Education and Higher Education,
+                                The student obtains a certificate of attendance from the company equivalent to one year of experience in the labor market,
+                                Assisting the 
+                            </p>
+						</div>
+
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+                            <h4>offline Training program</h4><br/>
+							<a href=""><h4>تقديم طلب</h4></a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+	<!--================End Features Area =================-->
+
+
+	<!--================Footer Area =================-->
+<div id="Contact-US">
+	<footer class="footer_area">
+		<div class="container">
+			<div class="row footer_inner">
+				<div class="col-lg-5 col-sm-6">
+					<aside class="f_widget ab_widget">
+						<div class="f_title">
+							<h3>Contact	Us</h3>
+						</div>
+						<p>OKTech<br>
+							Call: 00962798718399<br>
+							Email: 	OK@OKTech-Jo.com
+							<br><br>
+							All rights reserver This template is made  with OKTech
+							</p>
+					</aside>
+				</div>
+				<div class="col-lg-5 col-sm-6">
+					<aside class="f_widget news_widget">
+						<div class="f_title">
+							<h3>Our Location</h3>
+						</div>
+						<p>Jordan University Street - Patrol Signal - Jamal Awad Al-Awamleh Complex - Fifth Floor
+							</p>
+							<div class="mapouter"><div class="gmap_canvas"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6767.131159602135!2d35.915727!3d31.999790000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x72778baced1fdd3d!2sOKTech%20For%20Technology%2C%20Training%20%26%20E-Solution!5e0!3m2!1sen!2sjo!4v1651966514097!5m2!1sen!2sjo" width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><a href="https://www.fnfgo.com/">Friday Night Funkin Mods</a></div><style>.mapouter{position:relative;text-align:right;width:400px;height:150px;}.gmap_canvas {overflow:hidden;background:none!important;width:400px;height:150px;}.gmap_iframe {width:400px!important;height:150px!important;}</style></div>
+					</aside>
+				</div>
+				<div class="col-lg-2">
+					<aside class="f_widget social_widget">
+						<div class="f_title">
+							<h3>Follow Me</h3>
+						</div>
+						<p>Let us be social</p>
+						<ul class="list">
+							<li><a href="https://web.facebook.com/OKTECHegypt/?_rdc=1&_rdr
+								" target="_blanck"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="https://twitter.com/oktech_lighting?lang=en
+								" target="_blanck"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="
+								" target="_blanck"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+	<!--================End Footer Area =================-->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="OKTech/js/jquery-3.2.1.min.js"></script>
+	<script src="OKTech/js/popper.js"></script>
+	<script src="OKTech/js/bootstrap.min.js"></script>
+	<script src="OKTech/js/stellar.js"></script>
+	<script src="OKTech/js/jquery.magnific-popup.min.js"></script>
+	<script src="OKTech/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="OKTech/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+	<script src="OKTech/vendors/isotope/isotope-min.js"></script>
+	<script src="OKTech/vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="OKTech/js/jquery.ajaxchimp.min.js"></script>
+	<script src="OKTech/vendors/counter-up/jquery.waypoints.min.js"></script>
+	<script src="OKTech/vendors/counter-up/jquery.counterup.min.js"></script>
+	<script src="OKTech/js/mail-script.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="OKTech/js/gmaps.min.js"></script>
+	<script src="OKTech/js/theme.js"></script>
+</body>
+
+</html>
