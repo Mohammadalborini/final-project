1e96154a1d3f145f4c9c2a78e8048f4a2ee47cf0
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-08 00:55:31 +0300

    add and delete files

---
+++ e-practical training/Tuned Applications.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="Tuned Applications/Images/Images/Website Logo.jpg" type="image/png">
+	<title>Tuned Applications</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="Tuned Applications/css/bootstrap.css">
+	<link rel="stylesheet" href="Tuned Applications/vendors/linericon/style.css">
+	<link rel="stylesheet" href="Tuned Applications/css/font-awesome.min.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="Tuned Applications/css/style.css">
+</head>
+
+<body>
+
+	<!--================Header Menu Area =================-->
+	<header class="header_area">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand-logo_h" href="Tuned Applications.php"><img src="Tuned Applications/Images/Images/Website Logo.jpg" alt=""></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav justify-content-center">
+							<li class="nav-item active"><a class="nav-link" href="Tuned Applications.php">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="#About-us">About Us</a></li>
+							<li class="nav-item"><a class="nav-link" href="#Our-Server">Our Serves</a></li>
+							<li class="nav-item submenu dropdown">
+								<a href="#Our-Works" class="nav-link dropdown-toggle">Our Works</a>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#Our-Team" class="nav-link dropdown-toggle">Our Team</a>
+							</li>
+							<li class="nav-item"><a class="nav-link" href="#Contact-US">Contact US</a></li>
+							
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--================Header Menu Area =================-->
+	
+	<!--================Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5">
+						<div class="banner_content">
+                            <p>Tuned Applications is a Corporate Training, Software, Recruitment & Consulting Company since 2011.</p>
+							<h2>
+								Tuned Applications
+							</h2>
+				
+							<div class="d-flex align-items-center">
+								<a class="primary_btn" href="#About-us"><span>More Information</span></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-7">
+						<div class="home_right_img">
+							<img class="img-fluid" src="Tuned Applications/img/banner/home-right.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Home Banner Area =================-->
+
+	<!--================Start Features Area =================-->
+	<div id="About-us">
+	<section class="section_gap features_area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<p class="top_title">We Have Exclusive Stunning Features</p>
+						<h2>About Us</h2>
+                        <h1>Description</h1>
+						<p style="color: black;">Tuned Applications is a Corporate Training, Software, Recruitment & Consulting Company since 2011.
+                            Tuned Applications Company is one of the most professional and trustworthy companies in Jordan in the area of the IT field, offering a comprehensive range of training, developmental solutions, consultation, and support to both individuals and corporate professionals locally and regionally.
+                            It was established in 2011 and operates as a fully prepared training corporate.
+							</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<div class="left_features">
+						<img class="img-fluid" src="Tuned Applications/Images/Images/course.jpg" alt="">
+					</div>
+				</div>
+				<div class="col-lg-5 offset-lg-1">
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+
+							<h4>The Courses</h4>
+                            <p style="color: black;">
+                                Java<br/>
+                                android<br/>
+                                python<br/>
+                                Oracle<br/>
+                                cyber security<br/>
+                                asp net mvc<br/>
+                                Flutter<br/>
+                                QA
+                            </p>
+						</div>
+
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+							<h4>Certificates</h4>
+                            <p style="color: black;">
+                                1-certification degree on pyhton<br/>
+                                2-certification degree on java<br/>
+                                3-certification degree on android<br/>
+                                4-certification degree on oracle<br/>
+                                5-certification degree on cyber securtiy<br/>
+                                6-certification degree on asp .net mvc<br/>
+                                7-certification degree on flutter
+                            </p>
+						</div>
+
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+                            <h4>Oflline Training program 250,00 Dinar</h4><br/>
+							<a href=""><h4>تقديم طلب</h4></a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+	<!--================End Features Area =================-->
+
+
+	<!--================Footer Area =================-->
+<div id="Contact-US">
+	<footer class="footer_area">
+		<div class="container">
+			<div class="row footer_inner">
+				<div class="col-lg-5 col-sm-6">
+					<aside class="f_widget ab_widget">
+						<div class="f_title">
+							<h3>Contact	Us</h3>
+						</div>
+						<p>Tuned Applications<br>
+							Call: 07 8988 0205<br>
+							Email: http://www.tunedapps.com/
+							<br><br>
+							All rights reserver This template is made  with Tuned Applications
+							</p>
+					</aside>
+				</div>
+				<div class="col-lg-5 col-sm-6">
+					<aside class="f_widget news_widget">
+						<div class="f_title">
+							<h3>Our Location</h3>
+						</div>
+						<p>Al Shakhshir Complex, Al Shaheed St 21, Amman
+							</p>
+							<div class="mapouter"><div class="gmap_canvas"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13536.084174372252!2d35.900815!3d31.987445!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfc419774a851ab00!2sTuned%20Applications!5e0!3m2!1sen!2sjo!4v1651958986421!5m2!1sen!2sjo" width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><a href="https://www.fnfgo.com/">Friday Night Funkin Mods</a></div><style>.mapouter{position:relative;text-align:right;width:400px;height:150px;}.gmap_canvas {overflow:hidden;background:none!important;width:400px;height:150px;}.gmap_iframe {width:400px!important;height:150px!important;}</style></div>
+					</aside>
+				</div>
+				<div class="col-lg-2">
+					<aside class="f_widget social_widget">
+						<div class="f_title">
+							<h3>Follow Me</h3>
+						</div>
+						<p>Let us be social</p>
+						<ul class="list">
+							<li><a href="https://web.facebook.com/TunedApps/
+								" target="_blanck"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="https://twitter.com/search?q=TunedApps
+								" target="_blanck"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="https://www.instagram.com/TunedApps/
+								" target="_blanck"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+	<!--================End Footer Area =================-->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="Tuned Applications/js/jquery-3.2.1.min.js"></script>
+	<script src="Tuned Applications/js/popper.js"></script>
+	<script src="Tuned Applications/js/bootstrap.min.js"></script>
+	<script src="Tuned Applications/js/stellar.js"></script>
+	<script src="Tuned Applications/js/jquery.magnific-popup.min.js"></script>
+	<script src="Tuned Applications/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="Tuned Applications/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+	<script src="Tuned Applications/vendors/isotope/isotope-min.js"></script>
+	<script src="Tuned Applications/vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="Tuned Applications/js/jquery.ajaxchimp.min.js"></script>
+	<script src="Tuned Applications/vendors/counter-up/jquery.waypoints.min.js"></script>
+	<script src="Tuned Applications/vendors/counter-up/jquery.counterup.min.js"></script>
+	<script src="Tuned Applications/js/mail-script.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="Tuned Applications/js/gmaps.min.js"></script>
+	<script src="Tuned Applications/js/theme.js"></script>
+</body>
+
+</html>
