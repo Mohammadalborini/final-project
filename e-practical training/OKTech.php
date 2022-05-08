81e40ff2b1138135512be43ef866c7e01accb0e2
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-08 19:51:03 +0300

    edit

--- e-practical training/OKTech.php
+++ e-practical training/OKTech.php
@@ -142,7 +142,7 @@
 					<div class="single_feature">
 						<div class="feature_head">
 							<span class="lnr lnr-screen"></span>
-                            <h4>offline Training program</h4><br/>
+                            <h4>offline Training program (for free)</h4><br/>
 							<a href=""><h4>تقديم طلب</h4></a>
 						</div>
 
