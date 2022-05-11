306a4efcc01eeff0632a0672b3d26437c7b37b85
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-11 22:01:00 +0300

    add file sc

--- e-practical training/indexs.php
+++ e-practical training/indexs.php
@@ -195,7 +195,7 @@
                      </div>
                   </div>
                   <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                     <div class="header_information">
+                     <div class="header_information" style="background: #3bac49;">
                         <nav class="navigation navbar navbar-expand-md navbar-dark ">
                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                            <span class="navbar-toggler-icon"></span>
@@ -230,7 +230,7 @@
     
       <!--  footer -->
       <footer>
-         <div class="footer">
+         <div class="footer" style="background: #3bac49;">
             <div class="copyright">
                <div class="container">
                   <div class="row">
