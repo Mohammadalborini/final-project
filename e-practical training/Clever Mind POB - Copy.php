aefc2f519d7db5b52bede2444cc8f03d30fc067c
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-17 23:10:32 +0300

    add three pages

---
+++ e-practical training/Clever Mind POB - Copy.php
@@ -0,0 +1,247 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="Clever Mind POB/Images\Images/Website Logo.png" type="image/png">
+	<title>Clever Mind POB</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="Clever Mind POB/css/bootstrap.css">
+	<link rel="stylesheet" href="Clever Mind POB/vendors/linericon/style.css">
+	<link rel="stylesheet" href="Clever Mind POB/css/font-awesome.min.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="Clever Mind POB/css/style.css">
+</head>
+
+<body>
+
+	<!--================Header Menu Area =================-->
+	<header class="header_area">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand-logo_h" href="Clever Mind POB.php"><img src="Clever Mind POB/Images\Images/Website Logo.png" alt=""></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav justify-content-center">
+							<li class="nav-item active"><a class="nav-link" href="Clever Mind POB.php">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="#About-us">About Us</a></li>
+							<li class="nav-item"><a class="nav-link" href="#Our-Server">Our Serves</a></li>
+							<li class="nav-item submenu dropdown">
+								<a href="#Our-Works" class="nav-link dropdown-toggle">Our Works</a>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#Our-Team" class="nav-link dropdown-toggle">Our Team</a>
+							</li>
+							<li class="nav-item"><a class="nav-link" href="#Contact-US">Contact US</a></li>
+							
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--================Header Menu Area =================-->
+	
+	<!--================Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5">
+						<div class="banner_content">
+							<h2>
+								Clever Mind POB
+							</h2>
+							<p>
+								With over 5 years of experience, helping businesses to grow
+							</p>
+							<div class="d-flex align-items-center">
+								<a class="primary_btn" href="#About-us"><span>More Information</span></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-7">
+						<div class="home_right_img">
+							<img class="img-fluid" src="Clever Mind POB/img/banner/home-right.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Home Banner Area =================-->
+
+	<!--================Start Features Area =================-->
+	<div id="About-us">
+	<section class="section_gap features_area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<p class="top_title">We Have Exclusive Stunning Features</p>
+						<h2>About Us</h2>
+                        <h1>Description</h1>
+						<p style="color: black;">Our Vision for Clever Mind ICT is to
+                            become number one Tech
+                            Solutions & Training innovators
+                            company that provides Web &
+                            Mobile Solutions and , Software Solutions And Smart drives
+                            for corporate,
+                            professionals and students in
+                            Jordan and Middle East.
+							</p>
+					</div>
+				</div>
+			</div>
+
+            <div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<h2>More Details</h2>
+						<p style="color: black;">The training consists of two stages, the first stage is a theoretical course on (the basics of training and project building) QA and the second stage is practical (depends on building and developing projects from the simplest project to the largest project). Online training only from two to three months,
+                             for students, graduates, employees, etc. There is no accreditation for all university students for university training. Training will be 3-4 days a week. The goal of the training is for the student to gain strong practical experience in the field of training and work on many projects to prove his skills. <br/><br/>
+                             As for the follow-up with the student: the training consists of an explanation of recorded files, recorded lectures, practical applications, strong projects, and in some lectures via Skype or Zoom. Private training means following up with each student on a case-by-case basis so that we can identify weaknesses, address them, and strengthen you in the entire field of training.
+							</p>
+					</div>
+				</div>
+			</div>
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<div class="left_features">
+						<img class="img-fluid" src="Clever Mind POB/Images\Images\Our Servese.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-5 offset-lg-1">
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+
+							<h4>The Courses</h4>
+                            <p style="color: black;">
+                                1- Web Training Languages: HTML, CSS, JavaScript, PHP<br/>
+                                2- Android Training Languages: Java Using Android studio.<br/>
+                                3- IOS Training Languages: Swift Using XCode "Must have MAC Laptop"
+                            </p>
+						</div>
+
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+							<h4>Certificates</h4>
+                            <p style="color: black;">
+                                1- Certificate of the theoretical course<br/>
+                                2- Certificate of training experience<br/>
+                                3- Certificate of thanks and appreciation<br/>
+                                4 - Team Work Certificate<br/>
+                                5- A certificate of one year experience for outstanding students. During this period, the student will be fully capable of working on any project in his field.
+                            </p>
+						</div>
+
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+                            <h4>Online Training program 50,000 dinars</h4>
+						
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+			<div class="row justify-content-center">
+						<a href="" ><h2 style="cursor: pointer;">تقديم طلب</h2></a>
+			</div>
+		</div>
+	</section>
+</div>
+	<!--================End Features Area =================-->
+
+
+	<!--================Footer Area =================-->
+<div id="Contact-US">
+	<footer class="footer_area">
+		<div class="container">
+			<div class="row footer_inner">
+				<div class="col-lg-5 col-sm-6">
+					<aside class="f_widget ab_widget">
+						<div class="f_title">
+							<h3>Contact	Us</h3>
+						</div>
+						<p>Clever Mind POB ICT<br>
+							Call: +962-792-848-931 - Clever Care Team<br>
+							Email: Clevermindpob@gmail.com
+							<br><br>
+							All rights reserver This template is made  with Cleve Mind POB
+							</p>
+					</aside>
+				</div>
+				<div class="col-lg-5 col-sm-6">
+					<aside class="f_widget news_widget">
+						<div class="f_title">
+							<h3>Our Location</h3>
+						</div>
+						<p>Amman-Jordan -King Hussein Business Park<br>
+							ZINC -23
+							</p>
+							<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=400&amp;height=150&amp;hl=en&amp;q=clever mind pob&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.fnfgo.com/">Friday Night Funkin Mods</a></div><style>.mapouter{position:relative;text-align:right;width:400px;height:150px;}.gmap_canvas {overflow:hidden;background:none!important;width:400px;height:150px;}.gmap_iframe {width:400px!important;height:150px!important;}</style></div>
+					</aside>
+				</div>
+				<div class="col-lg-2">
+					<aside class="f_widget social_widget">
+						<div class="f_title">
+							<h3>Follow Me</h3>
+						</div>
+						<p>Let us be social</p>
+						<ul class="list">
+							<li><a href="https://www.facebook.com/ClevermindICT/
+								" target="_blanck"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="https://twitter.com/search?q=cleverMindICT
+								" target="_blanck"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="https://www.instagram.com/clevermindpob/
+								" target="_blanck"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+	<!--================End Footer Area =================-->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="Clever Mind POB/js/jquery-3.2.1.min.js"></script>
+	<script src="Clever Mind POB/js/popper.js"></script>
+	<script src="Clever Mind POB/js/bootstrap.min.js"></script>
+	<script src="Clever Mind POB/js/stellar.js"></script>
+	<script src="Clever Mind POB/js/jquery.magnific-popup.min.js"></script>
+	<script src="Clever Mind POB/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="Clever Mind POB/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+	<script src="Clever Mind POB/vendors/isotope/isotope-min.js"></script>
+	<script src="Clever Mind POB/vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="Clever Mind POB/js/jquery.ajaxchimp.min.js"></script>
+	<script src="Clever Mind POB/vendors/counter-up/jquery.waypoints.min.js"></script>
+	<script src="Clever Mind POB/vendors/counter-up/jquery.counterup.min.js"></script>
+	<script src="Clever Mind POB/js/mail-script.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="Clever Mind POB/js/gmaps.min.js"></script>
+	<script src="Clever Mind POB/js/theme.js"></script>
+</body>
+
+</html>
