05a9c53f8c217a5fd358e973e189aa79a64ba054
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-17 22:11:00 +0300

    ADD IMGAE

--- e-practical training/admin/Admin page/header.php
+++ e-practical training/admin/Admin page/header.php
@@ -32,14 +32,14 @@ $username = $_SESSION['username'];
 
 <body class="">
   <div class="wrapper ">
-    <div class="sidebar" data-color="azure" data-background-color="black" data-image="../assets/img/sidebar-2.jpg">
+    <div class="sidebar" data-color="azure" data-background-color="black" >
       <!--
         Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
         Tip 2: you can also add an image using data-image tag
     -->
       <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
-         Admin Site
+         <img src="../assets/img/header icon.png" width="270px" height="100px">
         </a></div>
       <div class="sidebar-wrapper">
         <ul class="nav">
