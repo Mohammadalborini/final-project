184f6a67a180fcb962ad2508a285fcf348906d3c
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-28 17:18:19 +0300

    edit

--- e-practical training/company offers.php
+++ e-practical training/company offers.php
@@ -15,7 +15,7 @@ include("coniction.php");
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>المساعدة في ايجاد شركة</title>
+      <title>تقديم عروض الشركات</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
