d2d0b77c67811e253e60e358e42635abbba891f7
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-05-28 16:39:47 +0300

    edit

--- e-practical training/company offers.php
+++ e-practical training/company offers.php
@@ -40,90 +40,102 @@ $student_id = $_SESSION['Student_id'];
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
       <style>
-.form-control {
-	height: 40px;
-	box-shadow: none;
-	color: #969fa4;
-}
-.form-control:focus {
-	border-color: #5cb85c;
-}
-.form-control, .btn {        
-	border-radius: 3px;
-}
-.signup-form {
-	width: 450px;
-	margin: 0 auto;
-	padding: 30px 0;
-  	font-size: 15px;
-}
-.signup-form h2 {
-	color: #636363;
-	margin: 0 0 15px;
-	position: relative;
-	text-align: center;
-}
-.signup-form h2:before, .signup-form h2:after {
-	content: "";
-	height: 2px;
-	width: 30%;
-	background: #d4d4d4;
-	position: absolute;
-	top: 50%;
-	z-index: 2;
-}	
-.signup-form h2:before {
-	left: 0;
-}
-.signup-form h2:after {
-	right: 0;
-}
-.signup-form .hint-text {
-	color: #999;
-	margin-bottom: 30px;
-	text-align: center;
-}
-.signup-form form {
-	color: #999;
-	border-radius: 3px;
-	margin-bottom: 15px;
-	background: #f2f3f7;
-	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-	padding: 30px;
-}
-.signup-form .form-group {
-	margin-bottom: 20px;
-}
-.signup-form input[type="checkbox"] {
-	margin-top: 3px;
-}
-.signup-form .btn {        
-	font-size: 16px;
-	font-weight: bold;		
-	min-width: 140px;
-	outline: none !important;
-}
-.signup-form .row div:first-child {
-	padding-right: 10px;
-}
-.signup-form .row div:last-child {
-	padding-left: 10px;
-}    	
-.signup-form a {
-	color: #fff;
-	text-decoration: underline;
-}
-.signup-form a:hover {
-	text-decoration: none;
-}
-.signup-form form a {
-	color: #5cb85c;
-	text-decoration: none;
-}	
-.signup-form form a:hover {
-	text-decoration: underline;
-}  
 
+div, form, input, label, p { 
+      padding: 0;
+      margin: 0;
+      outline: none;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 14px;
+      color: #666;
+      line-height: 22px;
+      }
+      legend{
+         text-align: right;
+      }
+      h1 {
+      position: absolute;
+      margin: 0;
+      font-size: 40px;
+      color: #fff;
+      z-index: 2;
+      line-height: 83px;
+      }
+      form {
+      width: 50%;
+      padding: 20px;
+      border-radius: 6px;
+      background: #fff;
+      box-shadow: 0 0 8px #006622;
+      margin-left: 25%; 
+      }
+      input {
+      margin-bottom: 10px;
+      border: 1px solid #ccc;
+      border-radius: 3px;
+      }
+      input {
+      width: calc(100% - 10px);
+      padding: 5px;
+      }
+      .item {
+      position: relative;
+      margin: 10px 0;
+      text-align: right;
+      }
+      .item span {
+      color: red;
+      }
+      .colums {
+      display:flex;
+      justify-content:space-between;
+      flex-direction:row;
+      flex-wrap:wrap;
+      }
+      .colums div {
+      width:100%;
+      }
+      .btn-block {
+      margin-top: 10px;
+      text-align: center;
+      }
+      button {
+      width: 150px;
+      padding: 10px;
+      border: none;
+      border-radius: 5px; 
+      background:  #006622;
+      font-size: 16px;
+      color: #fff;
+      cursor: pointer;
+      }
+      button:hover {
+      background:  #00b33c;
+      }
+      @media (min-width: 568px) {
+      .name-item, .city-item {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      }
+      .name-item input, .name-item div {
+      width: calc(50% - 20px);
+      }
+      .name-item div input {
+      width:97%;}
+      .name-item div label {
+      display:block;
+      padding-bottom:5px;
+      }
+      }
+      .collapse h2{
+     color: #fff;
+                                    font-size: 50px;
+                                    font-weight: bold;
+                                    line-height: 22px;
+                                    margin-right: 350px;
+                                    font-family: serif;
+}
       </style>
    </head>
    <!-- body -->
@@ -155,19 +167,7 @@ $student_id = $_SESSION['Student_id'];
                            <span class="navbar-toggler-icon"></span>
                            </button>
                            <div class="collapse navbar-collapse" id="navbarsExample04" >
-                           <?php
-			         $sql = "SELECT Fall_name FROM student where Student_id = $student_id ";
-				$result = mysqli_query($con, $sql);
-				if (mysqli_num_rows($result) > 0) {
-					while ($row = mysqli_fetch_assoc($result)) {
-							$text1 = $row['Fall_name'];
-							echo '<h1 style="margin-bottom: 55px;">';
-	   					    echo $text1;     
-						    echo '</h1>';  		
-							}
-						}
-				?>
-                              <div class="sign_btn"><a href="logout.php" style="margin-bottom: 55px;">Log out</a></div>
+                           <h2>تقديم عروض الشركات للتدريب</h2>
                            </div>
                         </nav>
                      </div>
@@ -179,36 +179,49 @@ $student_id = $_SESSION['Student_id'];
       <!-- end header inner -->
       <!-- end header -->
 
-      <!--  contact -->
+      <!--  form -->
+      <div class="testbox">
+      <form method="POST" action="">
+        <fieldset>
+          <legend>الرجاء من الشركات ادخال كافة المعلومات</legend>
+           <div class="colums">
+          <div class="item" >
+            <label > أسم الشركة<span>*</span></label>
+            <input type="text" name="name" required/>
+          </div>
+          <div class="item">
+            <label > الدورات التي تقدمها<span>*</span></label>
+            <input type="text" name="cours" required/>
+          </div>
+            <div class="item">
+            <label >طريقة التدريب (offline/online)<span>*</span></label>
+            <input type="text"   name="method" required/>
+          </div>
+         <div class="item">
+            <label >نوع التدريب (course/company)<span>*</span></label>
+            <input type="text"   name="type" required/>
+          </div>
+          <div class="item">
+            <label >الشهادات التي تقدمها<span>*</span></label>
+            <input type="text"   name="Certi" required/>
+          </div>
+          <div class="item">
+            <label >موقع الشركة<span>*</span></label>
+            <input type="text"   name="location" required/>
+          </div>
+          <div class="item">
+            <label >Profile picture<span>*</span></label>
+            <input type="file"  name="file" required/>
+          </div>
+                </fieldset>
+        <div class="btn-block">
+          <button type="submit" name="submit" href="/">Submit</button>
+        </div>
+      </form>
+    </div>
+     
 
-      <div class="signup-form">
-    <form action="" method="POST" enctype="multipart/form-data">
-		<h2>إرسال الموافقة </h2>
-		<p class="hint-text">(PDF) الرجاء التأكد من تعبئة البيانات بشكل صحيح وإرفاق ملف بصيغة  </p>
-        <div class="form-group">
-			<div class="row">
-				<div class="col"><input type="text" class="form-control" name="name" placeholder="اسم الطالب" value="<?php echo $text1; ?>" required="required" style="text-align: right;" readonly></div>
-			</div>        	
-        </div>
-        <div class="form-group">
-        	<input type="text" class="form-control" name="id" placeholder="الرقم الجامعي" value="<?php echo $student_id; ?>" required="required" style="text-align: right;" readonly>
-        </div>
-		<div class="form-group">
-            <input type="text" class="form-control" name="Spec" placeholder="التخصص" required="required" style="text-align: right;">
-        </div>
-		<div class="form-group">
-            <input type="text" class="form-control" name="company" placeholder="اسم الشركة" required="required" style="text-align: right;">
-        </div>   
-        <div class="form-group">
-            <input type="file" name="file" required="required">
-        </div>       
-		<div class="form-group">
-            <button type="submit" name="submit" class="btn btn-success btn-lg btn-block">إرسال</button>
-        </div>
-    </form>
-</div>
-
-      <!-- end contact -->
+      <!-- end form -->
       <!--  footer -->
       <footer>
          <div class="footer" >
