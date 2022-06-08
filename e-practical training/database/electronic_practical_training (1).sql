d385e33edd4cfe3aa0ec1b6b6ce11b2c123c78a9
Mohammad Alnajjar <mohamadalnajjar2000@gmail.com>
2022-06-08 23:46:58 +0300

    edit

--- e-practical training/database/electronic_practical_training (1).sql
+++ e-practical training/database/electronic_practical_training (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2022 at 04:15 AM
+-- Generation Time: Jun 08, 2022 at 12:15 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -52,6 +52,7 @@ CREATE TABLE `companies` (
   `The_Course` varchar(512) NOT NULL,
   `Training_method` varchar(256) NOT NULL,
   `Location` varchar(256) NOT NULL,
+  `Loca_map` text NOT NULL,
   `Training_type` varchar(256) NOT NULL,
   `Certificates` varchar(256) NOT NULL,
   `programming_languages` varchar(256) NOT NULL,
@@ -63,18 +64,18 @@ CREATE TABLE `companies` (
 -- Dumping data for table `companies`
 --
 
-INSERT INTO `companies` (`id`, `name`, `The_Course`, `Training_method`, `Location`, `Training_type`, `Certificates`, `programming_languages`, `imagename`, `Exfile`) VALUES
-(1, 'Clever Mind POB', 'Web Developer,\r\nMobile Application(android,IOS),\r\nQA and PM', 'Online', 'JO, 11181, Al-Sha\'b St, Amman', 'course', '1-Theoretical course certificate\r\n2-Training Experience Certificate\r\n3-Certificate of Thanks and Appreciation', 'html,css,Js,jquery,php,mysql,swift', 'icon1', 'png'),
-(2, 'Tuned Applications', 'Java,android,python,Oracle,cyber security,asp net mvc,Flutter,QA', 'offline(in company)', 'Al Shakhshir Complex, Al Shaheed St 21, Amman', 'course', '1-certification degree on pyhton\r\n2-certification degree on java\r\n3-certification degree on android\r\n4-certification degree on oracle\r\n5-certification degree on cyber securtiy\r\n6-certification degree on asp .net mvc\r\n7-certification degree on flutter', 'Java,android,python,Oracle,cyber security,asp .net mvc,Flutter,QA', 'icon2', 'jpg'),
-(3, 'OKTech', 'NETWORK basics course,\nCCNA Network Design and Management Course,\nMCSA Server Systems and Management Course,\nMICROSOFT EXCHANGING SERVER 2012 Course,\nComputer maintenance, software + hardware course,\nCCNA Network Design and Management Course,\nCCNA+ SECURITY . Network Security and Protection Course,\nKALI LINUX Penetration Testing Systems Course,\nCEH Ethical Penetration Testing Course', 'offline(in company)', 'Jordan University Street - Patrol Signal - Jamal Awad Al-Awamleh Complex - Fifth Floor', 'course', 'Issuing accredited certificates from Al-Balqa Applied University and the Ministry of Education and Higher Education,\r\nThe student obtains a certificate of attendance from the company equivalent to one year of experience in the labor market,\r\nAssisting the ', 'CCNA,SECURITY,KALI LINUX,CEH,CCNP', 'icon3', 'png'),
-(4, 'Ayyash', 'Cyber security', 'offline', 'Gaza/Palestine', 'Company', ' Cyber security certificate', 'Cyber security', 'image2', 'jpg'),
-(5, 'Mohammad ', 'Web Developer, Mobile Application', 'offline', 'amman/jordan', 'course', 'Web Developer certificate, Mobile Application certificate', 'html,css,js,php,mysql,java', 'image0', 'jpg'),
-(6, 'Ali', 'Web Developer, Mobile Application, Oracle', 'online', 'amman/jordan', 'course', 'Web Developer certificate, Mobile Application certificate, Oracle certificate', 'Java,php,html,css,js,mysql,Oracle', 'image0', 'jpg'),
-(7, 'Ahmmad', 'Web Developer, Mobile Application, Oracle, Java', 'offline', 'Amman', 'course', 'Web Developer certificate, Mobile Application certificate, Oracle certificate, Java certificate', 'Java,Oracle', 'image0', 'jpg'),
-(8, 'Company1', 'java, python', 'offline', 'Amman', 'Company', 'java certificate, python certificate', 'Java,python', 'image0', 'jpg'),
-(9, 'Company2', 'flitter, siwft', 'offline', 'Amman', 'course', 'flitter certificate, swift certificate', 'flitter, siwft', 'image1', 'jpg'),
-(10, 'Company3', 'c++, c', 'offline', 'Amman', 'course', 'c++ certificate, c certificate', 'c++, c', 'image7', 'jpg'),
-(11, 'Company4', 'java, python, flitter', 'offline', 'Amman', 'Company', 'java certificate, python certificate, flitter certificate', 'java, python, flitter', 'image9', 'jpg');
+INSERT INTO `companies` (`id`, `name`, `The_Course`, `Training_method`, `Location`, `Loca_map`, `Training_type`, `Certificates`, `programming_languages`, `imagename`, `Exfile`) VALUES
+(1, 'Clever Mind POB', 'Web Developer,\r\nMobile Application(android,IOS),\r\nQA and PM', 'Online', 'JO, 11181, Al-Sha\'b St, Amman', 'https://goo.gl/maps/xtWFivL8dNs8iYBZ8', 'course', '1-Theoretical course certificate\r\n2-Training Experience Certificate\r\n3-Certificate of Thanks and Appreciation', 'html,css,Js,jquery,php,mysql,swift', 'icon1', 'png'),
+(2, 'Tuned Applications', 'Java,android,python,Oracle,cyber security,asp net mvc,Flutter,QA', 'offline(in company)', 'Al Shakhshir Complex, Al Shaheed St 21, Amman', 'https://goo.gl/maps/XahafHPfiaBpZTDZ6', 'course', '1-certification degree on pyhton\r\n2-certification degree on java\r\n3-certification degree on android\r\n4-certification degree on oracle\r\n5-certification degree on cyber securtiy\r\n6-certification degree on asp .net mvc\r\n7-certification degree on flutter', 'Java,android,python,Oracle,cyber security,asp .net mvc,Flutter,QA', 'icon2', 'jpg'),
+(3, 'OKTech', 'NETWORK basics course,\nCCNA Network Design and Management Course,\nMCSA Server Systems and Management Course,\nMICROSOFT EXCHANGING SERVER 2012 Course,\nComputer maintenance, software + hardware course,\nCCNA Network Design and Management Course,\nCCNA+ SECURITY . Network Security and Protection Course,\nKALI LINUX Penetration Testing Systems Course,\nCEH Ethical Penetration Testing Course', 'offline(in company)', 'Jordan University Street - Patrol Signal - Jamal Awad Al-Awamleh Complex - Fifth Floor', 'https://goo.gl/maps/UettafzyHK8Jpp7W9', 'course', 'Issuing accredited certificates from Al-Balqa Applied University and the Ministry of Education and Higher Education,\r\nThe student obtains a certificate of attendance from the company equivalent to one year of experience in the labor market,\r\nAssisting the ', 'CCNA,SECURITY,KALI LINUX,CEH,CCNP', 'icon3', 'png'),
+(4, 'Ayyash', 'Cyber security', 'offline', 'Gaza/Palestine', 'https://goo.gl/maps/57AJdtKuRMXDNSbv7', 'Company', ' Cyber security certificate', 'Cyber security', 'image2', 'jpg'),
+(5, 'Mohammad ', 'Web Developer, Mobile Application', 'offline', 'amman/jordan', 'https://goo.gl/maps/8QRRczAARYB5q7G47', 'course', 'Web Developer certificate, Mobile Application certificate', 'html,css,js,php,mysql,java', 'image0', 'jpg'),
+(6, 'Ali', 'Web Developer, Mobile Application, Oracle', 'online', 'amman/jordan', 'https://goo.gl/maps/8QRRczAARYB5q7G47', 'course', 'Web Developer certificate, Mobile Application certificate, Oracle certificate', 'Java,php,html,css,js,mysql,Oracle', 'image0', 'jpg'),
+(7, 'Ahmmad', 'Web Developer, Mobile Application, Oracle, Java', 'offline', 'Amman', 'https://goo.gl/maps/8QRRczAARYB5q7G47', 'course', 'Web Developer certificate, Mobile Application certificate, Oracle certificate, Java certificate', 'Java,Oracle', 'image0', 'jpg'),
+(8, 'Company1', 'java, python', 'offline', 'Amman', 'https://goo.gl/maps/8QRRczAARYB5q7G47', 'Company', 'java certificate, python certificate', 'Java,python', 'image0', 'jpg'),
+(9, 'Company2', 'flitter, siwft', 'offline', 'Amman', 'https://goo.gl/maps/8QRRczAARYB5q7G47', 'course', 'flitter certificate, swift certificate', 'flitter, siwft', 'image1', 'jpg'),
+(10, 'Company3', 'c++, c', 'offline', 'Amman', 'https://goo.gl/maps/8QRRczAARYB5q7G47', 'course', 'c++ certificate, c certificate', 'c++, c', 'image7', 'jpg'),
+(11, 'Company4', 'java, python, flitter', 'offline', 'Amman', 'https://goo.gl/maps/8QRRczAARYB5q7G47', 'Company', 'java certificate, python certificate, flitter certificate', 'java, python, flitter', 'image9', 'jpg');
 
 -- --------------------------------------------------------
 
@@ -88,15 +89,19 @@ CREATE TABLE `companies_databases` (
   `student_id` char(10) NOT NULL,
   `email` varchar(256) NOT NULL,
   `specialization` varchar(256) NOT NULL,
-  `training_type` varchar(256) NOT NULL
+  `training_type` varchar(256) NOT NULL,
+  `evaluation1` varchar(10) NOT NULL DEFAULT 'no value',
+  `evaluation2` varchar(10) NOT NULL DEFAULT 'no value',
+  `evaluation3` varchar(10) NOT NULL DEFAULT 'no value',
+  `secret_assessment` varchar(256) NOT NULL DEFAULT 'no value'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `companies_databases`
 --
 
-INSERT INTO `companies_databases` (`id`, `student_name`, `student_id`, `email`, `specialization`, `training_type`) VALUES
-(1, 'محمد عبد الحكيم توفيق عبدالله', '3180605071', '3180605071@std.wise.edu.jo', 'هندسة برمجيات', 'full stack web');
+INSERT INTO `companies_databases` (`id`, `student_name`, `student_id`, `email`, `specialization`, `training_type`, `evaluation1`, `evaluation2`, `evaluation3`, `secret_assessment`) VALUES
+(1, 'محمد عبد الحكيم توفيق عبدالله', '3180605071', '3180605071@std.wise.edu.jo', 'هندسة برمجيات', 'full stack web', '75%', '85%', '80%', 'no value');
 
 -- --------------------------------------------------------
 
@@ -110,6 +115,7 @@ CREATE TABLE `companies_offers` (
   `The_Course` varchar(512) NOT NULL,
   `Training_method` varchar(256) NOT NULL,
   `Location` varchar(256) NOT NULL,
+  `map` text NOT NULL,
   `Training_type` varchar(256) NOT NULL,
   `Certificates` varchar(256) NOT NULL,
   `email` varchar(256) NOT NULL,
@@ -122,8 +128,8 @@ CREATE TABLE `companies_offers` (
 -- Dumping data for table `companies_offers`
 --
 
-INSERT INTO `companies_offers` (`id`, `name`, `The_Course`, `Training_method`, `Location`, `Training_type`, `Certificates`, `email`, `programming_languages`, `imagename`, `Exfile`) VALUES
-(18, 'Company5', 'java', 'online', 'Amman', 'course', 'java certificate', 'company4@gmail.com', 'java', 'image11', 'jpg');
+INSERT INTO `companies_offers` (`id`, `name`, `The_Course`, `Training_method`, `Location`, `map`, `Training_type`, `Certificates`, `email`, `programming_languages`, `imagename`, `Exfile`) VALUES
+(18, 'Company5', 'java', 'online', 'Amman', 'https://goo.gl/maps/4n5MaNTsqr6WHHJf7', 'course', 'java certificate', 'company4@gmail.com', 'java', 'image11', 'jpg');
 
 -- --------------------------------------------------------
 
@@ -151,7 +157,7 @@ CREATE TABLE `student` (
 INSERT INTO `student` (`Student_id`, `Fall_name`, `Password`, `Specialization`, `Email`, `Academic_advisor`, `The_number_of_hours_spent`, `Degree`, `imagename`, `Acte`) VALUES
 ('3180601046', 'علي اسحاق عبدالرحمن القواسمه', 'UFUH912345', 'علم حاسوب', '3180601046@std.wise.edu.jo', 'صلاح الغيالين', 81, 'بكالوريوس', 'Ali', 'jpg'),
 ('3180605071', 'محمد عبدالحكيم توفيق عبدالله', 'UFUK921234', 'هندسة برمجيات', '3180605071@std.wise.edu.jo', 'عيسى علي العتوم', 120, 'بكالوريوس', 'Mohammad', 'jpg'),
-('3180605099', 'محمد حسن محمود عبدالله', 'UFUG912345', 'أمن شبكات', '3180605099@std.wise.edu.jo', 'اديب السعايدة', 115, 'بكالوريوس', 'thierd_student', 'png');
+('3180605099', 'محمد حسن محمود عبدالله', 'UFUG912345', 'أمن شبكات', '3180605099@std.wise.edu.jo', 'اديب السعايدة', 125, 'بكالوريوس', 'thierd_student', 'png');
 
 -- --------------------------------------------------------
 
